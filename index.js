b69226c895f1d8574bc8514da3dd78ec62361a34
Light0690 <106499341+Light0690@users.noreply.github.com>
2023-09-05 14:37:49 +0300

    add ports http

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-import express, { json } from "express";
+import express from "express";
 import cors from "cors";
 
-import * as users from "./users.json" assert { type: "json" };
+import { usersValidate } from "./validations/usersValidate.js";
+import { handleValidationErrors } from "./utils/handleValidationErrors.js";
+import { login } from "./controllers/UserController.js";
 
 const app = express();
 const PORT = 3001;
 
+app.use(express.json());
+app.use(cors());
+
 app.get("/", (req, res) => {
-  res.send("<h1>hell1o</h1>");
+  res.json("1");
 });
 
-app.listen(PORT, () => {
+app.post("/login", usersValidate, handleValidationErrors, login);
+
+app.listen(PORT, (err) => {
+  if (err) {
+    return console.log(err);
+  }
   console.log(`Server running on port ${PORT}`);
 });
